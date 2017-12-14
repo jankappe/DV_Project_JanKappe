858ecc11d83a04204612bc4df8dfc6be0893eac7
Jan <kappe.jan@hotmail.nl>
2017-12-14 17:08:24 +0100

    Added text and changed code for readability

--- Code/loaddata.js
+++ Code/loaddata.js
@@ -5,31 +5,42 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
 	var minYear = 2005
-	var maxYear = 2016
+	var maxYear = 2017
 	var testData = [['IMDB', 'Title year', 'Movie Title']];
 
-	var years = maxYear - minYear
-	for (var i = 0; i < years; i++) {
-		testData[0].push(String((minYear + i)))
-	}
 
-	movies.forEach(function(m) {
-		if(m.imdb_score != null && m.title_year >= minYear && m.title_year <= maxYear) 
-		{
-			tempArr = [parseFloat(m.imdb_score), m.title_year, m.movie_title]
-			for (var i = 0; i < years; i++) {
-				if(i == m.title_year - minYear)
-				{
-					tempArr.push(m.movie_facebook_likes)
-				}
-				else
-				{
-					tempArr.push(null)
-				}
-			}
-			testData.push(tempArr)
-		}	
-	});
+  // TEST
+
+  var columnData = new google.visualization.DataTable();
+  columnData.addColumn('number', 'IMDB Score');
+  columnData.addColumn('number', 'Title year');
+  columnData.addColumn({type: 'string', role: 'tooltip'});
+
+  var years = maxYear - minYear
+  for (var i = 0; i < years; i++) {
+    columnData.addColumn('number', String((minYear + i)))
+  }
+
+  movies.forEach(function(m) {
+    if(m.imdb_score != null && m.title_year >= minYear && m.title_year <= maxYear) 
+    {
+      var tempArr = [parseFloat(m.imdb_score), m.title_year, m.movie_title]
+      for (var i = 0; i < years; i++) {
+        if(i == m.title_year - minYear)
+        {
+          tempArr.push(m.movie_facebook_likes)
+        }
+        else
+        {
+          tempArr.push(null)
+        }
+      }
+      columnData.addRow(tempArr)
+    } 
+  });
+
+  var formatter = new google.visualization.NumberFormat(
+    {pattern:'####'});
 
 	//create a gradient palette using RainbowVis
     var rainbow = new Rainbow(); 
@@ -42,51 +53,51 @@ function drawChart() {
     	colorArr.push(rainbow.colorAt(i))
     }
 
-    var data = google.visualization.arrayToDataTable(testData);
+    // Create a dashboard.
+    var dashboard = new google.visualization.Dashboard(
+      document.getElementById('dashboard_div'));
 
-        // Create a dashboard.
-        var dashboard = new google.visualization.Dashboard(
-            document.getElementById('dashboard_div'));
-
-        // Create a range slider, passing some options
-        var donutRangeSlider = new google.visualization.ControlWrapper({
-          'controlType': 'NumberRangeFilter',
-          'containerId': 'filter_div',
-          'options': {
-            'filterColumnLabel': 'Title year'
+    // Create a range slider, passing some options
+    var donutRangeSlider = new google.visualization.ControlWrapper({
+        'controlType': 'NumberRangeFilter',
+        'containerId': 'filter_div',
+        'options': {
+          'filterColumnLabel': 'Title year',
+          'ui': {
+            'format': '####'
           }
-        });
-
-        var viewArr = [0]
-        for (var i = 0; i < years; i++) {
-        	viewArr.push(i+3)
         }
-        console.log(viewArr)
+    });
 
+    var viewArr = [0]
+    for (var i = 0; i < years; i++) {
+      viewArr.push(i+2)
+    }
 
         // Create a pie chart, passing some options
-        var scatter_chart = new google.visualization.ChartWrapper({
+    var scatter_chart = new google.visualization.ChartWrapper({
           'chartType': 'ScatterChart',
           'containerId': 'chart_div',
           'options': {
-            'width': 1300,
+            'width': 1200,
             'height': 550,
+            'chartArea': {'width': '80%', 'height': '90%'},
             'colors': colorArr,
             'dataOpacity': 0.8,
             'legend': 'right',
             'hAxis': {title: 'IMDB Score', minValue: 0, maxValue: 10},
-          	'vAxis': {title: 'Number of Facebook likes', minValue: 0, maxValue: 400000}
+          	'vAxis': {title: 'Number of Facebook likes', minValue: 0, maxValue: 400000},
           },
           'view': {'columns': viewArr}
-        });
+    });
 
         // Establish dependencies, declaring that 'filter' drives 'pieChart',
         // so that the pie chart will only display entries that are let through
         // given the chosen slider range.
-        dashboard.bind(donutRangeSlider, scatter_chart);
+    dashboard.bind(donutRangeSlider, scatter_chart);
 
         // Draw the dashboard.
-        dashboard.draw(testData);    
+    dashboard.draw(columnData);    
 
 
 }
